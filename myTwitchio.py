d7641fed23ef460617ddb02eb281593b86efb6a3
joshbla <jblakezsmith@gmail.com>
2022-07-06 17:08:06 -0600

    more commands

--- myTwitchio.py
+++ myTwitchio.py
@@ -5,7 +5,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        super().__init__(token='el2f20ocj68ggt0spwvugjpjwbpyfl', prefix='!', initial_channels=['libsbot'])
+        super().__init__(token='el2f20ocj68ggt0spwvugjpjwbpyfl', prefix='!', initial_channels=['doctorfoxmeat'])
 
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
@@ -14,13 +14,31 @@ class Bot(commands.Bot):
     
     @commands.command()
     async def hello(self, ctx: commands.Context):
-        # Send a hello back!
         await ctx.send(f'Hello {ctx.author.name}!')
     
     @commands.command()
     async def goodbye(self, ctx: commands.Context):
-        # Send a goodbye back!
         await ctx.send(f'Goodbye {ctx.author.name}!')
+    
+    @commands.command()
+    async def sex(self, ctx: commands.Context):
+        await ctx.send(f'Congratulations {ctx.author.name}, you\'re pregnant!')
+    
+    @commands.command()
+    async def gay(self, ctx: commands.Context):
+        await ctx.send(f'Congratulations {ctx.author.name}, you\'re gay!')
+
+    @commands.command()
+    async def die(self, ctx: commands.Context):
+        await ctx.send(f'Congratulations {ctx.author.name}, you\'re dead!')
+
+    @commands.command()
+    async def mods(self, ctx: commands.Context):
+        await ctx.send(f'No mods are currently installed.')
+
+    @commands.command()
+    async def test(self, ctx: commands.Context):
+        await ctx.send(f'Yup it works')
 
 
 bot = Bot()
