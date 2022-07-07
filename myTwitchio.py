206879a9489170b91994eeaafd911e7f05a4e954
joshbla <jblakezsmith@gmail.com>
2022-07-06 21:55:36 -0600

    Added test reply

--- myTwitchio.py
+++ myTwitchio.py
@@ -1,24 +1,33 @@
+import twitchio
 from twitchio.ext import commands
+import random
 
 
 class Bot(commands.Bot):
 
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        super().__init__(token='el2f20ocj68ggt0spwvugjpjwbpyfl', prefix='!', initial_channels=['Aliasbot'])
+        super().__init__(token='el2f20ocj68ggt0spwvugjpjwbpyfl', prefix='!', initial_channels=['libsbot'])
 
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
-    
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
-        await ctx.send(f'Hello {ctx.author.name}!')
-    
-    @commands.command()
-    async def goodbye(self, ctx: commands.Context):
-        await ctx.send(f'Goodbye {ctx.author.name}!')
+
+    async def event_message(self, message):
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
+        elif random.randint(0,5) == 0:
+            reply = "@" + message.author.name + " test successful"
+            await twitchio.Channel.send(message.channel, reply)
+        else:
+            return
+
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
     
     @commands.command()
     async def sex(self, ctx: commands.Context):
